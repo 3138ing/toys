a432fb972c4dce13e4b1a0e64c034e62fc8ebb81
3138ing <128033875+3138ing@users.noreply.github.com>
2023-03-28 16:36:14 +0900

    Update 2023-03-28_snippet_time.py

--- 2023-03-28_snippet_time.py
+++ 2023-03-28_snippet_time.py
@@ -1,6 +1,24 @@
-# 'timestamp(int)' to 'struct_time'
+import time
+
+timestamp_1 = 1677723600
+print(timestamp_1)
+
+# 'timestamp(int)' -> 'struct_time'
 local_time_1 = time.localtime(timestamp_1)
 UTC_1 = time.gmtime(timestamp_1)
+print(local_time_1, UTC_1)
 
-# 'struct_time' to 'formatted string'
-formatted_string_1 = strftime.strftime('%H:%M:%S', struct_time_1)
+# 'struct_time' -> 'formatted string'
+formatted_string_1 = time.strftime('%Y-%m-%d %H:%M:%S', local_time_1)
+print(formatted_string_1)
+
+
+import datetime
+
+# 'formatted string' -> int
+timestamp_1 = datetime.datetime.strptime(formatted_string_1, "%Y-%m-%d %H:%M:%S").timestamp()
+print(timestamp_1)
+
+# int -> 'formatted string'
+formatted_string_1 = datetime.datetime.fromtimestamp(timestamp_1).strftime('%Y-%m-%d %H:%M:%S')
+print(formatted_string_1)
