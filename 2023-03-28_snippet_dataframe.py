08061d159106573c4d0484a918fa0665b1fce44a
3138ing <128033875+3138ing@users.noreply.github.com>
2023-03-29 08:35:06 +0900

    Update and rename 2023-03-28_snippet_df to 2023-03-28_snippet_dataframe.py

---
+++ 2023-03-28_snippet_dataframe.py
@@ -0,0 +1,23 @@
+# options
+pd.set_option('display.max_rows', 500)
+
+# list -> df
+column_names_1 = ['column_name_1', 'column_name_2', 'column_name_3']
+list_1 = [['a', 1, 2], ['b', 3, 4], ['c', 5, 6], ['d', 7, 8]]
+df_1 = pd.DataFrame(list_1, columns=column_names_1)
+print(df_1)
+
+# index: column to index
+df_1.set_index('column_name_1', inplace=True)
+print(df_1)
+
+# index: 함수적용
+df_1.index = df_1.index.map(lambda x:x+x) 
+print(df_1)
+
+# column(new): 누적합(cumsum), 누적Max(cummax), 누적Min(cummin), 누적곱(cumprod)
+print(df_1['column_name_3'].cumsum())
+
+# 결측치
+df_1.fillna(0, inplace=True)
+
