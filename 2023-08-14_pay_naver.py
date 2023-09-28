c5d9de851f172c5540a97058cc97baf96ab7719a
3138ing <3138ing@gmail.com>
2023-09-28 14:21:38 +0900

    Update 2023-08-14_pay_naver.py

--- 2023-08-14_pay_naver.py
+++ 2023-08-14_pay_naver.py
@@ -24,28 +24,39 @@ def payopen(num):
                 print("PAYAMT", item)
                 break
 
-            part1 = '-'
-            part2 = '-'
+            part1 = '?'
+            part2 = '?'
+            part3 = '-'
             title = item["TITLE"]
-            description = item["DESCRIPTION"]
+            description1 = item["DESCRIPTION"]
+            description2 = '-'
 
-            title, part1, part2, description = pay.naver(title, part1, part2, description)
+            title, part1, part2, part3, description1, description2 = pay.naver(title, part1, part2, part3, description1, description2)
 
             data = []
             data.append("'" + item["PAYDTTM"][0:4])
             data.append("'" + item["PAYDTTM"][4:6])
             data.append("'" + item["PAYDTTM"][6:8])
             data.append(요일[datetime.datetime.strptime(item["PAYDTTM"][0:8], "%Y%m%d").weekday()])
-            data.append('하나')
-            data.append(part1)
-            data.append(part2)
             data.append(title)
-            data.append(description)
+            data.append(description1)
+            data.append(description2)
             data.append(format(item['PAYAMT'], ','))
+            data.append(part3)
+            data.append(part2)
+            data.append(part1)
+            data.append('하나')
+            data.append('-')
+            data.append('-')
+            data.append('-')
+            data.append('-')
+            
+            if data[0] != "'2023" or data[1] != "'05":
+                continue
 
             f.write('\t'.join(data) + '\n')
 
-            if title != "네이버파이낸셜" and description != '-':
+            if title != "네이버" and description1 not in ['-', '?']:
                 if title != "네이버페이충전":
                     print(data)
 
@@ -53,8 +64,9 @@ with open(filename_result, 'w') as f:
     pass
 
 start = 0
-end = 1
+end = 9
 
+#for i in range(end, start - 1, -1):
 for i in range(start, end+1):
     num = str(i)
     if num == '83':
