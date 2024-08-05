5ab4872c47832c05142419280b905c4f1129c529
3138ing <128033875+3138ing@users.noreply.github.com>
2024-08-05 10:58:29 +0900

    Update 2022-04-08_mp3_from_mp4.py

--- 2022-04-08_mp3_from_mp4.py
+++ 2022-04-08_mp3_from_mp4.py
@@ -1,11 +1,15 @@
+import sys
+sys.path.insert(0, '../toys-data')
+import conf
+
 import glob
 import os
 import ffmpeg
 
 #from moviepy.editor import *
 
-mp4_dir = 'f:\\word_pie_video'
-mp3_dir = 'f:\\word_pie_sound'
+mp4_dir = conf.WORDPIE_MP4_DIR
+mp3_dir = conf.WORDPIE_MP3_DIR
 
 mp4_list = glob.glob(os.path.join(mp4_dir, '*'))
 for mp4_fullpath in mp4_list:
@@ -14,7 +18,7 @@ for mp4_fullpath in mp4_list:
     mp3_fullpath = os.path.join(mp3_dir, mp4_filename.replace('.mp4', '.mp3'))
     try:
         print('=== Try ===', mp4_fullpath)
-        video = ffmpeg.input(mp4_fullpath).output(mp3_fullpath).run(cmd='C:\\Users\\xxx\\AppData\\Local\\Programs\\Python\\Python310\\Lib\\site-packages\\imageio_ffmpeg\\binaries\\ffmpeg-win64-v4.2.2.exe')
+        video = ffmpeg.input(mp4_fullpath).output(mp3_fullpath).run(cmd=conf.FFMPEG_EXE_PATH)
     except:
         print('=== Except ===')
         None
