acb2a0bd8311daca71154e379f0a3c982daa3764
3138ing <128033875+3138ing@users.noreply.github.com>
2024-08-08 14:28:14 +0900

    Delete 2023-08-29_pay.py

--- 2023-08-29_pay.py
+++
@@ -1,8 +0,0 @@
-def get_har_filename():
-    return ''
-
-def shinhan(title, part1, part2, description, splits):
-    return title, part1, part2, description
-
-def naver(title, part1, part2, part3, description1, description2):
-    return title, part1, part2, part3, description1, description2
