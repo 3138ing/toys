8ec0b87fe92229e33ed69207d3a8c2779818438f
3138ing <128033875+3138ing@users.noreply.github.com>
2025-02-07 10:27:24 +0900

    Update 2024-07-29_slack_alive.py

--- 2024-07-29_slack_alive.py
+++ 2024-07-29_slack_alive.py
@@ -5,10 +5,8 @@ import conf
 
 # %%
 import requests
-
-# %%
-def slack(title, content, ip):
-    requests.post(
+def slack_by_requests(title, content, ip):
+    response = requests.post(
         f'https://hooks.slack.com/services/{conf.SLACK_WEBHOOK_URL}',
         headers={
             'content-type': 'application/json'
@@ -26,6 +24,43 @@ def slack(title, content, ip):
             ]
         }
     )
+    for key, value in response.headers.items():
+        print(f"{key}: {value}")
+
+    print("\nBody:")
+    print(response.text)
+    print("HTTP Status Code:", response.status_code)
+
+# %%
+import subprocess
+import json
+import syslog
+def slack_by_curl(msg):
+    slack_webhook_url = f"https://hooks.slack.com/services/{conf.SLACK_WEBHOOK_URL}"
+    curl_command = [
+        "curl",
+        "-k", "-i",
+        "-X", "POST",
+        "-H", "Content-type: application/json",
+        "--data", json.dumps({"text": msg}),
+        slack_webhook_url
+    ]
+    try:
+        result = subprocess.run(curl_command, capture_output=True, text=True, check=True)
+        response = result.stdout
+        #
+        syslog.openlog(ident="slack", logoption=syslog.LOG_PID)
+        for line in response.split('\n'):
+            syslog.syslog(syslog.LOG_INFO, f"{line}")
+        syslog.closelog()
+    except subprocess.CalledProcessError as e:
+        syslog.openlog(ident="slack", logoption=syslog.LOG_PID)
+        syslog.syslog(syslog.LOG_ERR, f"[EXCEPTION] [CalledProcessError] {e.stderr}")
+        syslog.closelog()
+    except Exception as e:
+        syslog.openlog(ident="slack", logoption=syslog.LOG_PID)
+        syslog.syslog(syslog.LOG_ERR, f"[EXCEPTION] {e}")  
+        syslog.closelog()
 
 # %%
 def getmyip():
@@ -38,6 +73,5 @@ def getmyip():
             return ""
 
 # %%
-slack("굿모닝", ":sun_with_face: :smile: :herb: ", getmyip())
-
-
+#slack_by_requests("굿모닝", ":sun_with_face: :smile: :herb: ", getmyip())
+slack_by_curl(f":sun_with_face: :smile: :herb: {getmyip()}")
